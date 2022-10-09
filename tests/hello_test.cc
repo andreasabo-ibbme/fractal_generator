7666d05dfc047550b9f1166e58bcf686952da268
Andrea Sabo <andrea.sabo@mail.utoronto.ca>
2022-10-08 20:55:08 -0400

    Replace GoogleTest with Catch2

--- tests/hello_test.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(HelloTest, BasicAssertions_v2) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
